1525413e37b204cdb6b84eaf597c3f55f562acd7
Candillan <calvin.baker128@gmail.com>
2020-01-22 14:31:59 -0500

    fixed developer schema, added controller for devs

---
+++ app/controllers/developers_controller.rb
@@ -0,0 +1,47 @@
+class DevelopersController < ApplicationController
+    def index
+        @developers = Developer.all 
+    end
+
+    def show
+        @developer = Developer.find(params[:id])
+        @games = @developer.games
+    end
+
+    def new
+        @developer = Developer.new
+    end
+
+    def create
+        @developer = Developer.new(developer_params)
+        if @developer.save
+            redirect_to developer_path(@developer)
+        else 
+            flash[:errors] = @developer.errors.full_messages
+            redirect_to new_developer_path
+        end 
+    end
+
+    def edit
+        @developer = Developer.find(params[:id])
+    end
+
+    def update
+        @developer = Developer.find(params[:id])
+        if @developer.update(developer_params)
+            redirect_to developer_path(@developer)
+        else
+            flash[:errors] = @developer.errors.full_messages
+            redirect_to edit_developer_path(@developer)
+        end     
+    end
+
+    def delete
+    end
+
+    private 
+
+    def developer_params
+        params.require(:developer).permit(:username, :password, :age, :studio, :experience)
+    end
+end
