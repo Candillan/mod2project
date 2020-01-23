a4d6cba069f73d52fdaf68852391c099bb5de22c
kennja05 <jacobkenny05@gmail.com>
2020-01-23 14:01:10 -0500

    added some game model methods for analytics

--- app/models/game.rb
+++ app/models/game.rb
@@ -16,5 +16,57 @@ class Game < ApplicationRecord
         end 
     end 
 
+    def critic_review 
+        if self.reviews.count > 0
+            critic_review_scores = []
+            my_arr = self.reviews.select do |review|
+                review.user.critic
+            end
+            if my_arr.count < 0
+                return nil
+            else  
+                arr = my_arr.map do |review|
+                    review.rating.to_f
+                end
+                sum_arr = arr.reduce do |n, sum|
+                    n + sum
+                end
+                (sum_arr / arr.count).round(2)
+            end 
+        end 
+    end 
+
+    def audience_review
+        if self.reviews.count > 0 
+            my_arr = self.reviews.select do |review|
+                !review.user.critic
+            end 
+            if my_arr.length > 0 
+            arr = my_arr.map do |review|
+                review.rating.to_f
+            end
+            sum_arr = arr.reduce do |n, sum|
+                n + sum
+            end 
+            (sum_arr / arr.count).round(2)
+        end 
+        end 
+    end
+
+    def weighted_review
+        if !self.audience_review && !self.critic_review
+            return "This game has not been reviewed"
+        elsif
+            !self.audience_review && self.critic_review
+            return self.critic_review
+        elsif
+            self.audience_review && !self.critic_review
+            return self.audience_review
+        else 
+            weighted_sum = (self.critic_review * 0.65).round(2) + (self.audience_review * 0.35).round(2)
+        end 
+    end
+
+    
 
 end
