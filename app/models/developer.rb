9a9e3c3558412680df70c289f8ec342290a26b6a
kennja05 <jacobkenny05@gmail.com>
2020-01-24 10:48:28 -0500

    added methods for dev analytics

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -4,4 +4,23 @@ class Developer < ApplicationRecord
 
     validates :username, presence: true,  uniqueness: {case_sensitive: false}
 
+    #analytics methods
+
+    def self.total_devs
+        Developer.all.count
+    end 
+
+    def self.oldest_dev
+        Developer.all.max_by do |dev|
+            dev.age
+        end 
+    end 
+
+    def self.most_experienced_dev
+        Developer.all.max_by do |dev|
+            dev.experience 
+        end 
+    end 
+
+
 end
