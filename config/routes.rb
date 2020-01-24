b8e1702212a31d1e1c381c75187d645b4261cf6f
kennja05 <jacobkenny05@gmail.com>
2020-01-24 11:34:57 -0500

    added route route

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Rails.application.routes.draw do
   delete '/developers/:id', to: "developers#delete"
 
   #Sessions Routes
+  get '/', to: "sessions#new"
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
