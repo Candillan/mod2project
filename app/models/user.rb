5838aa752f9d4794151a37897b892f72f5f7e0c4
kennja05 <jacobkenny05@gmail.com>
2020-01-24 11:27:49 -0500

    added analytics methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,30 @@ class User < ApplicationRecord
     #may not want to have this validation in there during development
     #validates :password, length: { in: 6..15}
 
+    #analytics methods
+
+    def self.total_users
+        User.all.count
+    end 
+
+    def average_review
+        if self.reviews.count > 0 
+            running_total = 0
+            self.reviews.each do |review|
+                running_total += review.rating
+            end 
+            (running_total / self.reviews.count).to_f
+        else 
+            return 0
+        end 
+    end 
+
+    def self.harshest_critic
+        my_arr = User.all.sort do |user|
+                    user.average_review
+                end 
+        my_arr[-1]        
+    end 
+
     
 end
