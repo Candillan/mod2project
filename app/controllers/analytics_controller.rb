db834d3cc28d5fe3cb68a1e6ad65e04a299f7381
Candillan <calvin.baker128@gmail.com>
2020-01-24 11:03:28 -0500

    added complexity to analytics page

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,6 +6,12 @@ class AnalyticsController < ApplicationController
         @users = User.all
         @reviews = Review.all
         @developers = Developer.all
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end
+        if session[:developer_id]
+            @developer = Developer.find(session[:developer_id])
+        end
     end 
 
 end 
